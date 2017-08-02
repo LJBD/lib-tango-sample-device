9122e50b3493f1740d5ee31cb2df62aa3a517956
Lukasz Dudek <lukasz.j.dudek@uj.edu.pl>
2017-08-02 11:26:24 +0200

    Added a docstring to run_server.

--- many_classes/run_server.py
+++ many_classes/run_server.py
@@ -1,3 +1,27 @@
+"""
+This module can be used to launch a Tango Device Server with two Device
+Classes:
+
+* `DeviceOne`
+* `DeviceTwo`
+
+If you want to launch it just as a Python script, you'll have to add a
+Device Server called `run_server` to your Tango Database. It would look like
+that:
+
+>>> python run_server.py test
+
+In production-ready applications, it's strongly advised to include the `main`
+method of this module as an `entry_point` in a `setup.py` script.
+
+>>> setup(
+>>> (...)
+>>> entry_points={'console_scripts':['AggregateDS ='
+>>>                                  'your_package.run_server:main']}
+>>> (...)
+>>> )
+"""
+
 from many_classes.device_one import DeviceOne
 from many_classes.device_two import DeviceTwo
 from tango.server import run
