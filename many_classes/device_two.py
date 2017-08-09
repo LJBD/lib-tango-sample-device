0439b64729e09f27029b87d616d5aebd635346d6
Zbigniew Reszela <zreszela@cells.es>
2017-08-09 17:46:49 +0200

    Subscribe to some_attribute in device_two

--- many_classes/device_two.py
+++ many_classes/device_two.py
@@ -1,4 +1,4 @@
-from tango import DevState
+from tango import DevState, DeviceProxy, EventType, utils
 from tango.server import Device, attribute, DeviceMeta
 
 
@@ -14,5 +14,13 @@ class DeviceTwo(Device):
         return "Whatever"
 
     def init_device(self):
+        print "DeviceTwo.init_device: entering..."
+        d = DeviceProxy("sys/test/deviceone-01")
+        try:
+            id_ = d.subscribe_event("some_attribute",
+                                    EventType.CHANGE_EVENT,
+                                    utils.EventCallBack())
+        except Exception, e:
+            print e
         self.set_state(DevState.ON)
         self.set_status("Device 2 on")
